9d8035b58b17df3ed025e51761da4934be71cc50
ノブ <>
2021-09-24 22:04:06 +0900

    Avout.vue追加

---
+++ components/Nav.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <nav>
+        <nuxt-link to="/">ホーム</nuxt-link>
+        <nuxt-link to="/about">アバウト</nuxt-link>
+        </nav>
+    </div>
+</template>
---
+++ pages/about.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1 style="text-align: center;">About Page</h1>
+</template>
