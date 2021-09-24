e505004a65411633e1955ad2abe50d1baefd2495
ノブ <>
2021-09-24 23:18:37 +0900

    ユーザー一覧追加

--- components/Nav.vue
+++ components/Nav.vue
@@ -2,7 +2,8 @@
     <div>
         <nav>
         <nuxt-link to="/">ホーム</nuxt-link>
-        <nuxt-link to="/about">アバウト</nuxt-link>
+        <nuxt-link to="/about"  no-prefetch>アバウト</nuxt-link>
+        <nuxt-link :to="{ name: 'users-list' }">ユーザー一覧</nuxt-link>
         </nav>
     </div>
 </template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,3 +1,18 @@
 <template>
-    <h1 style="text-align: center;">About Page</h1>
+    <div>
+        <h1 style="text-align: center;">About Page</h1>
+        <h2>{{ subtitle }}</h2>
+    </div>
 </template>
+
+<script>
+export default {
+    layout: "original",
+    data(){
+            return {
+                subtitle: 'サブタイトルです'
+            }
+        },
+};
+
+</script>
