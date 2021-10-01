731e70b808efa9ab98ee81d91f747e87b19bb12c
ノブ <>
2021-10-01 12:49:46 +0900

    記事一覧、記事個別ページでasync,await実装

---
+++ pages/posts/_id.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1>記事個別ページ</h1>
+        <h2>{{ post.title }}</h2>
+        <p>{{ post.body }}</p>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+    async asyncData({ params }){
+        // console.log(context)
+        const { data } = await
+        axios.get('https://jsonplaceholder.typicode.com/posts/' + params.id)
+        return {post: data }
+    },
+}
+
+</script>
---
+++ pages/posts/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1>記事一覧</h1>
+        <ul>
+            <li v-for="post in posts" :key=post.id>
+                {{ post.title }}
+            </li>
+        </ul>
+    </div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    async asyncData(){
+        //asyncとawaitを使用してサーバーで処理してからクライアントへ返すようにしている
+        const responsePost = await
+            axios.get('https://jsonplaceholder.typicode.com/posts/')
+        const responseUser = await
+            axios.get('https://jsonplaceholder.typicode.com/users/')
+        return { posts: responsePost.data, users: responseUser.data }
+    },
+    // data(){
+    //     return{
+    //         posts: []
+    //     }
+    // },
+    // mounted(){
+    //     axios.get('https://jsonplaceholder.typicode.com/posts')
+    //         .then(respons => this.posts = respons.data)
+    // }
+}
+</script>
